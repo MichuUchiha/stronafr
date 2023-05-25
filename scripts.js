8fc0f28ce17f14233749aad41101adb071f29dab
MichuUchiha <127601599+MichuUchiha@users.noreply.github.com>
2023-05-25 16:40:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,1 @@
-/* -- Glow effect -- */
 
-/*const blob = document.getElementById("blob");
-
-window.onpointermove = (event) => {
-  const { clientX, clientY } = event;
-
-  blob.animate(
-    {
-      left: `${clientX}px`,
-      top: `${clientY}px`,
-    },
-    { duration: 3000, fill: "forwards" }
-  );
-};
-*/
